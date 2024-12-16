8e737e138f2c56999ebb28808b81b3729b54ca58
surakanK <surakan.aub@gmail.com>
2024-12-16 16:49:38 +0700

    init cms

---
+++ content/example.md
@@ -0,0 +1,6 @@
+---
+title: "Example Post"
+date: "2024-12-16"
+---
+
+This is an example Markdown file for testing Gatsby.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,27 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const adapter = require("gatsby-adapter-netlify").default;
+
 module.exports = {
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: false,
+    imageCDN: false,
+  }),
   siteMetadata: {
     title: `Asuras Web Admin`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     {
       resolve: `gatsby-omni-font-loader`,
       options: {
         enableListener: true,
-        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        preconnect: [
+          `https://fonts.googleapis.com`,
+          `https://fonts.gstatic.com`,
+        ],
         web: [
           {
             name: `Mitr`,
@@ -20,5 +30,17 @@ module.exports = {
         ],
       },
     },
-  ]
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+  ],
 };
